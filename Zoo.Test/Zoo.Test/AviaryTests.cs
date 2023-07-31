3788f06e0f3ecdce4b01400ccc1ce47625e5d6d1
AlexandraMudraya <alexandra.mudraya@mail.ru>
2023-07-31 22:07:26 +0300

    исправлено

--- Zoo.Test/Zoo.Test/AviaryTests.cs
+++ Zoo.Test/Zoo.Test/AviaryTests.cs
@@ -5,25 +5,34 @@ using Zoo;
 {
     public class AviaryTests
     {
-
-
-        //Aviary bigAviary = new Aviary("для слонов", "тропики", 500);
-
-        //List<AbstractAnimals> elephant = new List<AbstractAnimals>()
-        // {
-        //    new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Мира", 300, 4, "трубить хоботом"),
-        //    new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Боря", 300, 5, "трубить хоботом")
-        // };
-
-        //ElephantAnimals elephantFaina = new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "аууф", "Фаина", 300, 4, "трубить хоботом");
-
-        //List<AbstractAnimals> newElephant;
-
-        [TestCase(elephant, elephantFaina, newElephant)]
-        public void PlusAnimalTest(List<AbstractAnimals> animals, AbstractAnimals animal, List<AbstractAnimals> expected)
+        [TestCase]
+        public void PlusAnimalTest()
         {
-            List<AbstractAnimals> actual = Aviary.PlusAnimal(animals, animal);
-            Assert.AreEqual(expected, actual);
+            Aviary bigAviary = new Aviary("для слонов", "тропики", 500);
+            List<AbstractAnimals> elephant = new List<AbstractAnimals>()
+         {
+            new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Мира", 300, 4, "трубить хоботом"),
+            new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Боря", 300, 5, "трубить хоботом")
+         };
+            ElephantAnimals elephantFaina = new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "аууф", "Фаина", 300, 4, "трубить хоботом");
+
+            List<AbstractAnimals> actual = bigAviary.PlusAnimal(elephant, elephantFaina);
+            Assert.AreEqual(elephant.ToString(), actual.ToString());
+        }
+
+        [TestCase]
+        public void MinusAnimalTest()
+        {
+            Aviary bigAviary = new Aviary("для слонов", "тропики", 500);
+            List<AbstractAnimals> elephant = new List<AbstractAnimals>()
+         {
+            new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Мира", 300, 4, "трубить хоботом"),
+            new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Боря", 300, 5, "трубить хоботом"),
+            new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "аууф", "Фаина", 300, 4, "трубить хоботом")
+         };
+
+            List<AbstractAnimals> actual = bigAviary.MinusAnimal(elephant, elephant[1]);
+            Assert.AreEqual(elephant.ToString(), actual.ToString());
         }
     }
 }
