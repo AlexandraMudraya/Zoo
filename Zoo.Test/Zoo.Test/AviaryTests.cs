1fb805631f0e698a294126a001232067f21fb81a
AlexandraMudraya <alexandra.mudraya@mail.ru>
2023-07-31 20:14:50 +0300

    исправлено

--- Zoo.Test/Zoo.Test/AviaryTests.cs
+++ Zoo.Test/Zoo.Test/AviaryTests.cs
@@ -7,17 +7,17 @@ using Zoo;
     {
 
 
-        Aviary bigAviary = new Aviary("для слонов", "тропики", 500);
+        //Aviary bigAviary = new Aviary("для слонов", "тропики", 500);
 
-        List<AbstractAnimals> elephant = new List<AbstractAnimals>()
-         {
-            new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Мира", 300, 4, "трубить хоботом"),
-            new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Боря", 300, 5, "трубить хоботом")
-         };
+        //List<AbstractAnimals> elephant = new List<AbstractAnimals>()
+        // {
+        //    new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Мира", 300, 4, "трубить хоботом"),
+        //    new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "ауф", "Боря", 300, 5, "трубить хоботом")
+        // };
 
-        ElephantAnimals elephantFaina = new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "аууф", "Фаина", 300, 4, "трубить хоботом");
+        //ElephantAnimals elephantFaina = new ElephantAnimals("слон", "тропики", 70, "сено", "травоядный", "аууф", "Фаина", 300, 4, "трубить хоботом");
 
-        List<AbstractAnimals> newElephant;
+        //List<AbstractAnimals> newElephant;
 
         [TestCase(elephant, elephantFaina, newElephant)]
         public void PlusAnimalTest(List<AbstractAnimals> animals, AbstractAnimals animal, List<AbstractAnimals> expected)
